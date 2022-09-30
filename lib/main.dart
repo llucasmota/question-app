7abe6265cca04e652adc4425e649bbaf30861ac0
llucasmota <lucas.o.mota@gmail.com>
2022-09-29 21:51:09 -0300

    feat(main): adjustment of question limit

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,38 @@ import './GeneralButton.dart';
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (haveASelectedAnswer) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  final _perguntas = const [
+    {
+      'texto': 'Qual sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+    },
+    {
+      'texto': 'Qual seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+    },
+    {
+      'texto': 'Qual seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro']
+    }
+  ];
+
+  bool get haveASelectedAnswer {
+    print('$_perguntaSelecionada ${_perguntas.length}');
+    return _perguntaSelecionada + 1 <= _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
-      },
-      {
-        'texto': 'Qual seu animal favorito?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
-      },
-      {
-        'texto': 'Qual seu instrutor favorito?',
-        'respostas': ['Maria', 'João', 'Leo', 'Pedro']
-      }
-    ];
-
-    List<String> respostas =
-        perguntas[_perguntaSelecionada].cast()['respostas'];
+    List<String> respostas = haveASelectedAnswer
+        ? _perguntas[_perguntaSelecionada].cast()['respostas']
+        : [];
     // for (String textResposta
     //     in perguntas[_perguntaSelecionada].cast()['respostas']) {
     //   respostas.add(GeneralButton(textResposta, _responder));
@@ -42,12 +50,14 @@ class _PerguntaAppState extends State<PerguntaApp> {
       appBar: AppBar(
         title: const Text('Perguntas'),
       ),
-      body: Column(
-        children: <Widget>[
-          Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-          ...respostas.map((t) => GeneralButton(t, _responder)).toList()
-        ],
-      ),
+      body: haveASelectedAnswer
+          ? Column(
+              children: <Widget>[
+                Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                ...respostas.map((t) => GeneralButton(t, _responder)).toList()
+              ],
+            )
+          : null,
     ));
   }
 }
